07e1d846bd9e319abefb452727efd88cc3c4f48d
ae86zhizhi <550149470@qq.com>
2017-05-31 14:03:51 +0800

    修正从数据库建立索引

--- anotherlucenedemo/search/src/search/MysqlFacility.java
+++ anotherlucenedemo/search/src/search/MysqlFacility.java
@@ -4,86 +4,91 @@ import com.mysql.jdbc.PreparedStatement;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-private static Connection getConn() {
-    String driver = "com.mysql.jdbc.Driver";
-    String url = "jdbc:mysql://localhost:3306/samp_db";
-    String username = "root";
-    String password = "";
-    Connection conn = null;
-    try {
-        Class.forName(driver); //classLoader,加载对应驱动
-        conn = (Connection) DriverManager.getConnection(url, username, password);
-    } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-    return conn;
-}
-private static int insert(Student student) {
-    Connection conn = getConn();
-    int i = 0;
-    String sql = "insert into students (Name,Sex,Age) values(?,?,?)";
-    PreparedStatement pstmt;
-    try {
-        pstmt = (PreparedStatement) conn.prepareStatement(sql);
-        pstmt.setString(1, student.getName());
-        pstmt.setString(2, student.getSex());
-        pstmt.setString(3, student.getAge());
-        i = pstmt.executeUpdate();
-        pstmt.close();
-        conn.close();
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-    return i;
-}
-private static int update(Student student) {
-    Connection conn = getConn();
-    int i = 0;
-    String sql = "update students set Age='" + student.getAge() + "' where Name='" + student.getName() + "'";
-    PreparedStatement pstmt;
-    try {
-        pstmt = (PreparedStatement) conn.prepareStatement(sql);
-        i = pstmt.executeUpdate();
-        System.out.println("resutl: " + i);
-        pstmt.close();
-        conn.close();
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-    return i;
-}
-private static Integer getAll() {
-    Connection conn = getConn();
-    String sql = "select * from students";
-    PreparedStatement pstmt;
-    try {
-        pstmt = (PreparedStatement)conn.prepareStatement(sql);
-        ResultSet rs = pstmt.executeQuery();
-        int col = rs.getMetaData().getColumnCount();
-        System.out.println("============================");
-        while (rs.next()) {
-            for (int i = 1; i <= col; i++) {
-                System.out.print(rs.getString(i) + "\t");
-                if ((i == 2) && (rs.getString(i).length() < 8)) {
-                    System.out.print("\t");
-                }
-             }
-            System.out.println("");
-        }
-            System.out.println("============================");
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-    return null;
-}
 public class MysqlFacility {
-    JDBCOperation.getAll();
-    JDBCOperation.insert(new Student("Achilles", "Male", "14"));
-    JDBCOperation.getAll();
-    JDBCOperation.update(new Student("Bean", "", "7"));
-    JDBCOperation.delete("Achilles");
-    JDBCOperation.getAll();
+	private static Connection getConn() {
+	    String driver = "com.mysql.jdbc.Driver";
+	    String url = "jdbc:mysql://localhost:3306/samp_db";
+	    String username = "root";
+	    String password = "";
+	    Connection conn = null;
+	    try {
+	        Class.forName(driver); //classLoader,加载对应驱动
+	        conn = (Connection) DriverManager.getConnection(url, username, password);
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return conn;
+	}
+	private static int insert(Student student) {
+	    Connection conn = getConn();
+	    int i = 0;
+	    String sql = "insert into students (Name,Sex,Age) values(?,?,?)";
+	    PreparedStatement pstmt;
+	    try {
+	        pstmt = (PreparedStatement) conn.prepareStatement(sql);
+	        pstmt.setString(1, student.getName());
+	        pstmt.setString(2, student.getSex());
+	        pstmt.setString(3, student.getAge());
+	        i = pstmt.executeUpdate();
+	        pstmt.close();
+	        conn.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return i;
+	}
+	private static int update(Student student) {
+	    Connection conn = getConn();
+	    int i = 0;
+	    String sql = "update students set Age='" + student.getAge() + "' where Name='" + student.getName() + "'";
+	    PreparedStatement pstmt;
+	    try {
+	        pstmt = (PreparedStatement) conn.prepareStatement(sql);
+	        i = pstmt.executeUpdate();
+	        System.out.println("resutl: " + i);
+	        pstmt.close();
+	        conn.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return i;
+	}
+	private static Integer getAll() {
+	    Connection conn = getConn();
+	    String sql = "select * from students";
+	    PreparedStatement pstmt;
+	    try {
+	        pstmt = (PreparedStatement)conn.prepareStatement(sql);
+	        ResultSet rs = pstmt.executeQuery();
+	        int col = rs.getMetaData().getColumnCount();
+	        System.out.println("============================");
+	        while (rs.next()) {
+	            for (int i = 1; i <= col; i++) {
+	                System.out.print(rs.getString(i) + "\t");
+	                if ((i == 2) && (rs.getString(i).length() < 8)) {
+	                    System.out.print("\t");
+	                }
+	             }
+	            System.out.println("");
+	        }
+	            System.out.println("============================");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+	public MysqlFacility(){
+		
+	}
+	public static void go(){
+		getAll();
+		insert(new Student("Achilles", "Male", "14"));
+		getAll();
+		update(new Student("Bean", "", "7"));
+		delete("Achilles");
+		getAll();
+	}
+    
 }
