cb6ce7a33e2e8a045212ce0a81c18b61fbb4252d
tianlian0 <liu13269455505@126.com>
2017-06-11 23:47:21 +0800

    更精细的地图
    增加了省市新闻列表页和导航栏

--- python web/test.py
+++ python web/test.py
@@ -38,6 +38,9 @@ def home():
     str=""
     for row in cur.fetchall():
         str+= printme("新闻：",row[0],row[1])
+
+    #cur.execute('select a.title,a.province from (select row_number()over(partition by menu_parentid order by province) as num,t.* from news t)a where num<=3')
+
     cur.close()
     db.close()
 
@@ -49,8 +52,26 @@ def home():
   <title>新闻地图</title>
   <script src="http://dreamspark.com.cn/echarts.js"></script>
   <script src="http://dreamspark.com.cn/china.js"></script>
+  <link rel="stylesheet" href="http://dreamspark.com.cn/public.css">
+      <link rel="stylesheet" href="http://dreamspark.com.cn/amazeui.min.css">
 </head>
 <body>
+
+<header class="am-topbar am-topbar-fixed-top wos-header">
+    <div class="am-container">
+        <h1 class="am-topbar-brand">
+            <a href="#"><img src="http://dreamspark.com.cn/images/logo.png" alt=""></a>
+        </h1>
+        <div class="am-collapse am-topbar-collapse" id="collapse-head">
+            <ul class="am-nav am-nav-pills am-topbar-nav">
+                <li class="am-active"><a href="#">首页</a></li>
+                <li><a href="http://news.sina.com.cn/">新浪新闻</a></li>
+                <li><a href="http://news.qq.com/">腾讯新闻</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+<br/>
 <div id="main" style="width: 1800px;height:800px;"></div>
 <script type="text/javascript">
     var myChart = echarts.init(document.getElementById('main'));
@@ -93,12 +114,12 @@ def home():
     };
 
     myChart.on('click', function (param){
-        var urlArr = ['http://www.baidu.com','http://www.baidu.com'];
+        var urlArr = ['http://www.baidu.com','/heilongjiang'];
         switch(param.name){
             case '河南':
                 location.href = urlArr[0];
                 break;
-            case '重庆':
+            case '黑龙江':
                 location.href = urlArr[1];
                 break;
             default:
@@ -112,11 +133,96 @@ def home():
 '''
     return partHead+str+partEnd
 
+def shengshiHead():
+    return'''
+    <!doctype html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+    <link rel="stylesheet" href="http://dreamspark.com.cn/amazeui.min.css">
+    <link rel="stylesheet" href="http://dreamspark.com.cn/public.css">
+
+    <script src="http://dreamspark.com.cn/jquery.min.js"></script>
+    <script src="http://dreamspark.com.cn/amazeui.min.js"></script>
+    
+    <body>
+
+<header class="am-topbar am-topbar-fixed-top wos-header">
+    <div class="am-container">
+        <h1 class="am-topbar-brand">
+            <a href="#"><img src="http://dreamspark.com.cn/images/logo.png" alt=""></a>
+        </h1>
+        <div class="am-collapse am-topbar-collapse" id="collapse-head">
+            <ul class="am-nav am-nav-pills am-topbar-nav">
+                <li><a href="/">首页</a></li>
+                <li class="am-active"><a href="#">省市新闻列表</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+
+    <div class="am-g">
+        <div class="am-u-sm-0 am-u-md-2 am-u-lg-3">&nbsp;</div>
+        <div class="am-u-sm-12 am-u-md-8 am-u-lg-6">
+            <div data-am-widget="list_news" class="am-list-news am-list-news-default ">
+                <div class="am-list-news-bd">
+                    <ul class="am-list">
+</head>
+    '''
+def shengshiEnd():
+    return'''
+                    </div>
+            </div>
+        </div>
+        <div class="am-u-sm-0 am-u-md-2 am-u-lg-3">&nbsp;</div>
+    </div>
+
+</body>
+</html>
+    '''
+
+def prinNewsDetail(title, context,link):
+    return '''
+
+                        <li class="am-g am-list-item-desced am-list-item-thumbed am-list-item-thumb-left">
+                             <div class="am-u-md-12 am-u-lg-6 userface">
+                                 <h3 class="am-list-item-hd"><a href="'''+link+'''">''' + title + '''</a></h3>
+                             </div>
+                            <div class=" am-u-sm-7 am-list-main">
+                                <div class="am-list-item-text">''' + context + '''</div>
+                            </div>
+                        </li>
+
+    '''
+
+
+@app.route('/heilongjiang', methods=['GET', 'POST'])
+def heilongjiang():
+    return shengshiHead()+prinNewsDetail("新闻标题1","新闻标题1","http://www.baidu.com/")+prinNewsDetail("新闻标题2","新闻标题2","http://www.baidu.com/")+'''
+                    </ul>
+                    <ul data-am-widget="pagination" class="am-pagination am-pagination-default" style="text-align: center">
+                        <li class="am-pagination-prev ">
+                            <a href="#" class="">上一页</a>
+                        </li>
+
+                        <li class="am-pagination-next ">
+                            <a href="#" class="">下一页</a>
+                        </li>
+
+                    </ul>
+    '''+shengshiEnd()
+
 @app.route('/signin', methods=['GET'])
 def signin_form():
     return '''<form action="/signin" method="post">
               <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
+              <p><input name= "password" type="password"></p>
               <p><button type="submit">Sign In</button></p>
               </form>'''
 
